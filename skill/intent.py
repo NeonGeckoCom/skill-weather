97d41b8f364d0f5604e9e18e1eefe41a5a889de2
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2021-09-15 16:01:35 -0700

    Cache location lookups per intent to speed up references (#7)

--- skill/intent.py
+++ skill/intent.py
@@ -14,8 +14,9 @@
 """Parse the intent into data used by the weather skill."""
 from datetime import timedelta
 
+import pytz
 from mycroft.util.time import now_local
-from neon_utils.location_utils import get_coordinates, get_location
+from neon_utils.location_utils import get_coordinates, get_location, get_timezone
 
 from .util import (
     get_utterance_datetime,
@@ -42,6 +43,22 @@ class WeatherIntent:
         self.language = language
         self.unit = message.data.get("unit")
         self.timeframe = CURRENT
+        self._location = None
+
+    def _get_location(self):
+        lat, lng = get_coordinates({"city": self.location})
+        city, county, state, country = get_location(lat, lng)
+        if not city and county:
+            city = f'{county} county'
+        if not city:
+            raise LocationNotFoundError(f"{self.location} could not be resolved")
+        self._location = {'city': city,
+                          'region': state,
+                          'country': country,
+                          'timezone': pytz.timezone(get_timezone(lat, lng)[0]),
+                          'latitude': lat,
+                          'longitude': lng
+                          }
 
     @property
     def geolocation(self):
@@ -51,28 +68,9 @@ class WeatherIntent:
         requested.  If the user asks "What is the weather in Russia"
         an error will be raised.
         """
-        lat, lng = get_coordinates({"city": self.location})
-        city, county, state, country = get_location(lat, lng)
-        if not city and county:
-            city = f'{county} county'
-        if not city:
-            raise LocationNotFoundError(f"{self.location} could not be resolved")
-        return {'city': city,
-                'region': state,
-                'country': country,
-                'timezone': None,  # TODO: Populate this from somewhere DM
-                'latitude': lat,
-                'longitude': lng
-                }
-        # if self._geolocation is None:
-        #     if self.location is None:
-        #         self._geolocation = dict()
-        #     else:
-        #         self._geolocation = get_geolocation(self.location)
-        #         if self._geolocation["city"].lower() not in self.location.lower():
-        #             raise LocationNotFoundError(self.location + " is not a city")
-        #
-        # return self._geolocation
+        if not self._location:
+            self._get_location()
+        return self._location
 
     @property
     def intent_datetime(self):
