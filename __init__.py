9a02451fdfed8330c3004e47b9f9d9e4fc407617
Åke Forslund <ake.forslund@gmail.com>
2021-11-24 07:54:29 +0100

    Fix query for humidity
    This fixes the usage of the dialog data for generating spoken report
    from humidity.

--- __init__.py
+++ __init__.py
@@ -578,8 +578,9 @@ class WeatherSkill(NeonSkill):
             dialog = get_dialog_for_timeframe(intent_data.timeframe, dialog_args)
             dialog.build_humidity_dialog()
             dialog.data.update(
-                humidity=self.translate(
-                    "percentage-number", data=dict(num=dialog.data["humidity"])
+                percent=self.translate(
+                    "percentage-number",
+                    data=dict(number=dialog.data["percent"])
                 )
             )
             self._speak_weather(dialog)
