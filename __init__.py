15257ff96fa41f55ec7da0e9f0426753ba9af05d
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2023-06-20 16:46:43 -0700

    Update homescreen weather (#41)
    * Troubleshooting homescreen updates
    
    * Update homescreen to use temp instead of feels_like

--- __init__.py
+++ __init__.py
@@ -1272,14 +1272,15 @@ class WeatherSkill(NeonSkill):
     @skill_api_method
     def get_current_weather_homescreen(self, msg: Optional[Message] = None):
         try:
-            LOG.info(f"Handling request for weather")
             config = self._get_weather_config(None)
             unit = config.unit_system
             coords = get_user_prefs()['location']
-            current = self.weather_api.get_current_weather_for_coordinates(unit, coords['lat'], coords['lng'])
+            LOG.info(f"Handling request for weather: {unit}, {coords}")
+            current = self.weather_api.get_current_weather_for_coordinates(
+                unit, coords['lat'], coords['lng'])
             condition = WeatherCondition(current["weather"][0])
             img_code = condition.image.replace("images/", "icons/")
-            current_weather = round(current["main"]["feels_like"])
+            current_weather = round(current["main"]["temp"])
             result = {"weather_code": img_code, "weather_temp": current_weather}
             if msg:
                 LOG.debug("Emitting weather response")
