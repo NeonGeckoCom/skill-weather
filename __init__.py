f7f77beedb5788a3f04f2b38e004db8f0dae734d
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2023-07-11 19:12:30 -0700

    Fix dialog error (#45)
    * Fix dialog error introduced in #44
    
    * Update logging to troubleshoot incorrect weather

--- __init__.py
+++ __init__.py
@@ -733,7 +733,7 @@ class WeatherSkill(NeonSkill):
                 gui_waiter.wait(10 - (time() - gui_start))
                 wait_for_signal_clear('isSpeaking')
                 self._display_more_current_conditions(weather, weather_location)
-            # dialog = CurrentDialog(intent_data, weather_config, weather.current)
+            dialog = CurrentDialog(intent_data, weather_config, weather.current)
             dialog.build_high_low_temperature_dialog()
             self._speak_weather(dialog)
             if self.gui.connected:
@@ -1287,7 +1287,7 @@ class WeatherSkill(NeonSkill):
             current_weather = round(current["main"]["temp"])
             result = {"weather_code": img_code, "weather_temp": current_weather}
             if msg:
-                LOG.debug("Emitting weather response")
+                LOG.debug(f"Emitting weather response: {result}")
                 self.bus.emit(msg.reply(
                     "skill-ovos-weather.openvoiceos.weather.response",
                     data={"report": result}))
