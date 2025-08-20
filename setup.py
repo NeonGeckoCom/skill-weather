53d283193ea0015c0e4cf938480b61204d922cb6
Daniel McKnight <daniel@neon.ai>
2025-08-20 15:46:58 -0700

    Update packages in setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from setuptools import setup
+from setuptools import find_packages, setup
 from os import getenv, path, walk
 
 SKILL_NAME = "skill-weather"
@@ -96,7 +96,7 @@ setup(
     author_email='developers@neon.ai',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=[SKILL_PKG, f"{SKILL_PKG}.skill"],
+    packages=find_packages(),
     package_data={SKILL_PKG: find_resource_files()},
     include_package_data=True,
     entry_points={"ovos.plugin.skill": PLUGIN_ENTRY_POINT}
